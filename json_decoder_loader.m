1c02ea02eecd3a29bf8c1e286ac189508d0eeeb2
dscott4 <davidkhscott@gmail.com>
2018-02-02 19:46:02 +0000

    oope
    update

--- json_decoder_loader.m
+++ json_decoder_loader.m
@@ -1,5 +1,7 @@
 val = json_decoder('AHU06 Supply Temp.json');
 data = val.values;
 c = struct2cell(data)'
+%c(:,3) = str2double(c(:,3));
+a = str2double(c(:,3))
 %plot(str2double(val.values.value));
 
