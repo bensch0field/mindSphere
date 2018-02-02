008812f9c0f8cb772d184c4a6a232d8a2309d899
dscott4 <davidkhscott@gmail.com>
2018-02-02 21:53:40 +0000

    lol
    update

--- preprocessing cleanup/ahu_gen.m
+++ preprocessing cleanup/ahu_gen.m
@@ -2,20 +2,20 @@ function AHU = ahu_gen()
 val = json_decoder('AHU06 Heating Coil.json');
 data = val.values;
 data = struct2cell(data)';
-AHU.driver = str2double(data(:,1));
+AHU.driver = str2double(data(:,3));
 
 val = json_decoder('AHU06 Heating Coil Feedback.json');
 data = val.values;
 data = struct2cell(data)';
-AHU.feedback = str2double(data(:,1));
+AHU.feedback = str2double(data(:,3));
 
 val = json_decoder('AHU06 Off Frost Temp.json');
 data = val.values;
 data = struct2cell(data)';
-AHU.frost_temp = str2double(data(:,1));
+AHU.frost_temp = str2double(data(:,3));
 
 val = json_decoder('AHU06 Supply Temp.json');
 data = val.values;
 data = struct2cell(data)';
-AHU.supply_temp = str2double(data(:,1));
+AHU.supply_temp = str2double(data(:,3));
 end
--- preprocessing cleanup/json_decoder_loader.m
+++ preprocessing cleanup/json_decoder_loader.m
@@ -1,12 +1,22 @@
 BMS = bms_gen();
-BMS_norm = data_normalisation([BMS.humidity1,BMS.humidity2,BMS.co2]);
+num = 16478;
+BMS_norm = data_normalisation([BMS.humidity1(1:num),BMS.humidity2(1:num),BMS.co2(1:num)]);
+figure
+plot(BMS_norm(:,:))
+legend('Humidity 1','Humidity 2','CO2')
 
 AHU = ahu_gen();
-
-%{
+AHU_norm = data_normalisation([AHU.driver,AHU.feedback,AHU.frost_temp,AHU.supply_temp]);
 figure
-plot(BMS_norm(:,1))
-hold on
-plot(BMS_norm(:,2))
-hold on
-%}
+plot(AHU_norm(:,:))
+legend('Driver','Feedback','Frost Temp','Supply Temp')
+
+[X,pca_net,network]=nlpca(AHU_norm',4,'pre_pca','yes',...
+    ... % 'pre_unit_variance','yes',... % not recommended for the star data
+    ... %'units_per_layer'      ,[12, 8, 3, 8, 12]   ,... % optional (to reduce data-dim from 19 to 12)
+    'weight_initialization','set_weights_randomly',...
+    'weight_decay_coefficient',0.01,...
+    'max_iteration',1000,...
+    'plotting','yes');
+
+
